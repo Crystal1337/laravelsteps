a4bc4cf2a52ec4c67da5420d74627d22e3d470cc
Crystal1337 <michal.nwk@gmail.com>
2020-09-12 18:34:24 +0200

    basics of MVC with migrations and layout 101

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,23 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
+use App\Post;
 class PostController extends Controller
 {
   function show($slug)
   {
-    $post = \DB::table('posts')->where('slug', $slug)->first();
 
-  //  $posts = [
-  //    'my-first-post' => 'Welcome. This is my first post',
-  //    'my-second-post' => 'Hello again. This is my second post'
-  //  ];
-  //  if(!array_key_exists($post, $posts))
-  //  {
-  //    abort(404, 'Sorry that post does not exist');
-  //  }
+    //  $posts = [
+    //    'my-first-post' => 'Welcome. This is my first post',
+    //    'my-second-post' => 'Hello again. This is my second post'
+    //  ];
+    //  if(!array_key_exists($post, $posts))
+    //  {
+    //    abort(404, 'Sorry that post does not exist');
+    //  }
+
+
+    //$post = DB::table('posts')->where('slug', $slug)->first();
+    //if(!$post)
+    //{
+    //  abort(404);
+    //}
+
+
     return view('post', [
-      'post' => $post
+      'post' => Post::where('slug', $slug)->firstOrFail()
     ]);
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contact', function () {
+    return view('contact');
+});
+
 Route::get('/test', function () {
     return view('test', [
       'name' => request('name')
